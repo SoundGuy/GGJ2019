23d7dfa9605012c2344db1cc4e6183637c859da2
Oded Sharon <git@odedsharon.com>
2020-01-31 15:30:39 +0200

    adding maze stuff and item holder

---
+++ Assets/Scripts/TriggerItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerItem : MonoBehaviour
+{
+    [SerializeField]
+    private Collider myCollider;
+    [SerializeField]
+    private string nameToRespondTo;
+
+    [SerializeField] private UnityEvent actions;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == nameToRespondTo)
+        {
+            Debug.Log("Responsing to " + other.gameObject.transform.parent.name);
+                actions.Invoke();
+        }
+    }
+}
